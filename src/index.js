8deac1f6100794b0d8b465ed50f68b77282fbc18
thanhaii <hailam@infevo.net>
2021-12-24 23:58:09 +0700

    static file, scss

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
+const { join } = require("path");
 
 const app = express();
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 //
 app.use(morgan("combined"));
