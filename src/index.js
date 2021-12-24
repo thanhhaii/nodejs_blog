74ad525bb81b1355851a620e79b1321857b28637
thanhaii <hailam@infevo.net>
2021-12-24 23:27:24 +0700

    template engine

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+
+const app = express();
+const port = 3000;
+
+//
+app.use(morgan("combined"));
+
+//Template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "\\resources\\views"));
+
+//__dirname = path of file index
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
